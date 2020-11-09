b6370f9c9fc6592f8cf8ffeae49e73b505d572d7
Georgios <test@example.com>
2020-11-09 23:44:57 +0100

    created portfolio html, css

---
+++ js/dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  document.addEventListener("click", function (e) {
+    var yatzyGame = document.getElementByClassName("yatzy-game");
+    window.location.href = "https://github.com/ulidin/my_projects/tree/master/Grupparbeten/Yatzy";
+  });
+});
---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+
+
+    document.addEventListener("click", function(e) {
+
+        let yatzyGame = document.getElementByClassName("yatzy-game");
+        window.location.href = "https://github.com/ulidin/my_projects/tree/master/Grupparbeten/Yatzy";
+
+    })
+
+
+})
